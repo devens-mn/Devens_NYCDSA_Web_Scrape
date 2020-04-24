094db85099a1f984b397e682dc08f494306027be
devens-mn <dadevens@gmail.com>
2020-04-24 08:52:17 -0500

    detmackrace.py - add sail trigger for new divison

--- detmackrace.py
+++ detmackrace.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+import time
 import re
+import csv
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,12 +11,13 @@ driver = webdriver.Chrome()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 
-# go to main TOC page to get years of race
+csv_file = open('detmacraceresults.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(csv_file)
+
+
 driver.get("https://www.bycmack.com/main_results.cfm")
 years = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/div/table/tbody/tr/td[7]/div/table/tbody/tr/td")
-# for yr in years[i].text:
-#   print(yr)
-# initialize array to hold urls for race results by years
+#https://www.bycmack.com/past-results/2017-overall.cfm
 race_rslts_urls = []
 for i in range(1, len(years)):
     # print(years[i].text)
@@ -22,12 +26,10 @@ for i in range(1, len(years)):
         url_hold = "https://www.bycmack.com/past-results/" + years[i].text[:4] +"-overall.cfm"
         # print(url_hold)
         race_rslts_urls = race_rslts_urls + [url_hold]
-#print out urls so I can know I formatted them correctly
-print(race_rslts_urls)
-
-# go to a given year's results
+# print(race_rslts_urls)
+# in 2019 they changed format so will ignore that year
+# print(race_rslts_urls[len(race_rslts_urls)-1])
 # driver.get('https://www.bycmack.com/past-results/2017-overall.cfm')
-
 # iterate from 2018 backward to end of list since 2019 had different format len(race_rslts_urls)
 for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
     print(race_rslts_urls[j])
@@ -41,22 +43,26 @@ for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
         else:
             continue
 
-    # find the year on the page so I don't have to pass it otherwise
     rc_year = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/b[1]")
-    print(rc_year.text)
-
-    # find and printrows of race results by entry / boat
+    # print(rc_year.text)
     rows = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr")
-    # iterate over rows in results page, if start at 1 get error, 2nd row is column names
-    for i in range(2, 100): # just do smallish number for now to avoid long waits with selenium
-        # don't collect column header row (sail no = sail)
-        # put in try to eliminate mistakes due to misformatting
+    # for i in range(1, len(rows)):
+    #     print(rows[i].text)
+    # set iterator for division header list to -1 since it trips right out
+    div_iter = -1
+    # try to return the fields from each row
+    for i in range(2, len(rows)+1):
+        #len(rows)+1
+        race_dict = {}
         try:
+            # print(i)
             sail_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[2]'
             sail = driver.find_element_by_xpath(sail_path)
             if sail.text != 'Sail':
                 boat_name_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[3]'
                 boat_name = driver.find_element_by_xpath(boat_name_path)
+                # design_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[5]'
+                # design = driver.find_element_by_xpath(design_path)
                 finish_time_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[5]'
                 finish_time = driver.find_element_by_xpath(finish_time_path)
                 elapse_time_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[6]'
@@ -67,10 +73,23 @@ for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
                 boat_class = driver.find_element_by_xpath(class_path)
                 div_plc_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[9]'
                 div_plc = driver.find_element_by_xpath(div_plc_path)
-                print(int(rc_year.text[:4]),' ',sail.text,' ',boat_name.text, ' ',finish_time.text,' ',\
-                elapse_time.text, ' ', corrected_time.text, ' ',boat_class.text,' ',div_plc.text)
+                # print(int(rc_year.text[:4]),' ',sail.text,' ',boat_name.text, ' ', finish_time.text, '')
             else:
+                div_iter = div_iter + 1
                 continue
         except:
             continue
+
+        race_dict['division'] = hold_lst[div_iter] 
+        race_dict['rc_year'] = rc_year.text[:5]
+        race_dict['sail'] = sail.text
+        race_dict['boat_name'] = boat_name.text
+    #     race_dict['design'] = design.text
+        race_dict['finish_time'] = finish_time.text
+        race_dict['elapse_time'] = elapse_time.text
+        race_dict['corrected_time'] = corrected_time.text
+        race_dict['boat_class'] = boat_class.text
+        race_dict['div_plc'] = div_plc.text
+        writer.writerow(race_dict.values())
+csv_file.close()
 driver.close()
