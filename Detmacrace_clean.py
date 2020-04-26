e9e8e83c31d9549fceaed602e69406ad2cf97d1f
devens-mn <dadevens@gmail.com>
2020-04-25 19:55:30 -0500

    detmackrace.py, Detmacrace_clean.py- detmackrace combined to write single csv, _clean removed merge step

--- Detmacrace_clean.py
+++ Detmacrace_clean.py
@@ -6,15 +6,10 @@ import numpy as np
 sail_data = pd.read_csv('detmacraceresults.csv', names=['Division_course', 'Year', \
     'Sail_No','Boat_name', 'Finish_time', 'Elapsed_Time', 'Corrected_Time',\
     'Boat_class', 'Division_placing'])
-sail_data_2019 = pd.read_csv('detmacraceresults_2019.csv', names=['Division_course',\
-    'Year', 'Sail_No','Boat_name', 'Finish_time', 'Elapsed_Time', 'Corrected_Time',\
-    'Boat_class', 'Division_placing'])
 
 #Duplicate elapsed time and corrected time columns without DNS, DNF, Retired for analysis
 sail_data['cln_elps_tm'] = sail_data['Elapsed_Time'].replace(['DNS', 'DNF', 'Retired'],np.nan)
 sail_data['cln_crrct_tm'] = sail_data['Corrected_Time'].replace(['DNS', 'DNF', 'Retired'],np.nan)
-sail_data_2019['cln_elps_tm'] = sail_data_2019['Elapsed_Time'].replace(['DNS', 'DNF', 'Retired'],np.nan)
-sail_data_2019['cln_crrct_tm'] = sail_data_2019['Corrected_Time'].replace(['DNS', 'DNF', 'Retired'],np.nan)
 
 #Define function to convert time strings to seconds for analysis
 def pdtime_cnvrt(dtafrm, timecolname):
@@ -34,21 +29,14 @@ def pdtime_cnvrt(dtafrm, timecolname):
 #use pdtime_cnvrt fxn to calculate correction time in seconds
 sail_data['crctn_in_sec'] = pdtime_cnvrt(sail_data, 'cln_elps_tm') - \
     pdtime_cnvrt(sail_data, 'cln_crrct_tm')
-sail_data_2019['crctn_in_sec'] = pdtime_cnvrt(sail_data_2019, 'cln_elps_tm') - \
-    pdtime_cnvrt(sail_data_2019, 'cln_crrct_tm')
 
 #Create new winsorized data files without outliers, found only 1 mistyped over 200,000
 sail_data_winsor = sail_data.loc[abs(sail_data['crctn_in_sec']) < 200000]
-sail_data_2019_winsor = sail_data_2019.loc[abs(sail_data['crctn_in_sec']) < 200000]
-
-#Merge original and winsorized data files
-merge_sail_winsor = pd.concat([sail_data_2019_winsor, sail_data_winsor], axis = 0)
-merge_sail = pd.concat([sail_data_2019, sail_data], axis = 0)
 
 #Divison and course name are merged, break out Division_only to allow analysis by division
-merge_sail_winsor['Division_only'] = merge_sail_winsor['Division_course'].str.slice(0,12)
-merge_sail['Division_only'] = merge_sail['Division_course'].str.slice(0,12)
+sail_data_winsor['Division_only'] = sail_data_winsor['Division_course'].str.slice(0,12)
+sail_data['Division_only'] = sail_data['Division_course'].str.slice(0,12)
 
 #write to csv file to pull back in for graphical analysis
-merge_sail_winsor.to_csv('merge_sail_winsor.csv', header=False, index=False)
-merge_sail.to_csv('merge_sail.csv', header=False, index=False)
+sail_data_winsor.to_csv('sail_data_winsor.csv', header=False, index=False)
+sail_data.to_csv('sail_data.csv', header=False, index=False)
--- detmackrace.py
+++ detmackrace.py
@@ -1,37 +1,88 @@
+# import selenium drivers, regex, csv to write the output
 from selenium import webdriver
 import time
 import re
 import csv
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 
+# start the webdriver
 driver = webdriver.Chrome()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 
+# open the csv file to write
 csv_file = open('detmacraceresults.csv', 'w', encoding='utf-8', newline='')
 writer = csv.writer(csv_file)
 
-
+# go to main results page, with links to all years
 driver.get("https://www.bycmack.com/main_results.cfm")
 years = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/div/table/tbody/tr/td[7]/div/table/tbody/tr/td")
-#https://www.bycmack.com/past-results/2017-overall.cfm
+# create list to hold urls 
 race_rslts_urls = []
 for i in range(1, len(years)):
-    # print(years[i].text)
     # get rid of years without overall finish formatted results
     if (re.search('[OAoa]', years[i].text) != None):
         url_hold = "https://www.bycmack.com/past-results/" + years[i].text[:4] +"-overall.cfm"
         # print(url_hold)
         race_rslts_urls = race_rslts_urls + [url_hold]
-# print(race_rslts_urls)
-# in 2019 they changed format so will ignore that year
-# print(race_rslts_urls[len(race_rslts_urls)-1])
-# driver.get('https://www.bycmack.com/past-results/2017-overall.cfm')
-# iterate from 2018 backward to end of list since 2019 had different format len(race_rslts_urls)
+
+# in 2019 they changed format so will collect from that indpendently and first
+driver.get("https://www.bycmack.com/past-results/2019-overall.cfm")
+rc_year = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/b[1]")
+# show progress
+print("https://www.bycmack.com/past-results/2019-overall.cfm")
+div_hdrs = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr/td/b")
+hold_lst = []
+for i in range(len(div_hdrs)):
+    if 'division' in div_hdrs[i].text.lower().split():
+        hold_lst = hold_lst + [div_hdrs[i].text]
+    else:
+        continue
+rows = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr")
+# try to return the fields from each row
+div_iter = 0
+for i in range(3, len(rows)+1):
+    race_dict = {}
+    try:
+        sail_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[2]'
+        sail = driver.find_element_by_xpath(sail_path)
+        if sail.text != 'Sail':
+            boat_name_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[3]'
+            boat_name = driver.find_element_by_xpath(boat_name_path)
+            # design_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[5]'
+            # design = driver.find_element_by_xpath(design_path)
+            finish_time_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[7]'
+            finish_time = driver.find_element_by_xpath(finish_time_path)
+            elapse_time_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[8]'
+            elapse_time = driver.find_element_by_xpath(elapse_time_path)
+            corrected_time_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[9]'
+            corrected_time = driver.find_element_by_xpath(corrected_time_path)
+            class_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[10]'
+            boat_class = driver.find_element_by_xpath(class_path)
+            div_plc_path = '/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr[' +str(i) + ']/td[11]'
+            div_plc = driver.find_element_by_xpath(div_plc_path)
+        else:
+            div_iter = div_iter + 1
+            continue
+    except:
+        continue
+    race_dict['division'] = hold_lst[div_iter] 
+    race_dict['rc_year'] = rc_year.text[:5]
+    race_dict['sail'] = sail.text
+    race_dict['boat_name'] = boat_name.text
+    # race_dict['design'] = design.text
+    race_dict['finish_time'] = finish_time.text
+    race_dict['elapse_time'] = elapse_time.text
+    race_dict['corrected_time'] = corrected_time.text
+    race_dict['boat_class'] = boat_class.text
+    race_dict['div_plc'] = div_plc.text
+    writer.writerow(race_dict.values())
+
+# now iterate from 2018 backward to end of list gathered in lines 20-30
 for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
+# show progress
     print(race_rslts_urls[j])
     driver.get(race_rslts_urls[j])
     # find division headers
@@ -44,7 +95,6 @@ for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
             continue
 
     rc_year = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/b[1]")
-    # print(rc_year.text)
     rows = driver.find_elements_by_xpath("/html/body/table[2]/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr")
     # for i in range(1, len(rows)):
     #     print(rows[i].text)
@@ -84,7 +134,6 @@ for j in range(1, len(race_rslts_urls)): #fill in with len race_rslts_urls
         race_dict['rc_year'] = rc_year.text[:5]
         race_dict['sail'] = sail.text
         race_dict['boat_name'] = boat_name.text
-    #     race_dict['design'] = design.text
         race_dict['finish_time'] = finish_time.text
         race_dict['elapse_time'] = elapse_time.text
         race_dict['corrected_time'] = corrected_time.text
